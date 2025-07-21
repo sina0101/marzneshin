756dbdbf9160d1797dfe5649d99c0bc006868c23
Sina Jafari <168327662+sina0101@users.noreply.github.com>
2025-07-21 19:13:04 +0330

    Update github-repo.query.ts

--- dashboard/src/features/github-repo/api/github-repo.query.ts
+++ dashboard/src/features/github-repo/api/github-repo.query.ts
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { projectInfo } from "@marzneshin/common/utils";
+import { projectInfo } from "@sina0101/common/utils";
 import { ofetch } from "ofetch";
 
 /**
